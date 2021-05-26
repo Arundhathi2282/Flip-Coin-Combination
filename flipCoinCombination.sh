8aed5d124e7dd725cbe8d0aa11ff5d68dc848938
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 23:33:59 +0530

    Add : Doublet combination and its percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -7,20 +7,41 @@ read -p "How many times you want to flip a coin : " num
 while [[ $flip -lt $num ]]
 do
 randomCheck1=$((RANDOM%2))
+randomCheck2=$((RANDOM%2))
 if [[ $randomCheck1 -eq $isHead ]]
 then
-	echo "Head is the winner"
-	dict[$x]=H
-	x=$(($x+1))
-	i=$(($i+1))
+	if [[ $randomCheck2 -eq $isHead ]]
+	then
+		echo "Head is the winner"
+		dict[$x]=HH
+		x=$(($x+1))
+		i=$(($i+1))
+	else
+		echo "Tail is the Winner"
+        	dict[$x]=HT
+        	x=$(($x+1))
+        	j=$(($j+1))
+		i=$(($i+1))
+	fi
 else
-	echo "Tail is the Winner"
-	dict[$x]=T
-	x=$(($x+1))
-	j=$(($j+1))
+	if [[ $randomCheck2 -eq $isHead ]]
+        then
+                echo "Head is the winner"
+                dict[$x]=HT
+                x=$(($x+1))
+                i=$(($i+1))
+		j=$(($j+1))
+        else
+                echo "Tail is the Winner"
+                dict[$x]=TT
+                x=$(($x+1))
+                j=$(($j+1))
+        fi
+
 fi
 flip=$(($flip+1))
 done
 echo ${dict[@]}
-per=`awk 'BEGIN{print ('$j' / 10) * 100}'`
+per=`awk 'BEGIN{print ('$j' / '$num') * 100}'`
 echo "Singlet percentage : $per"
+echo "Doublet percentage is : $per"
