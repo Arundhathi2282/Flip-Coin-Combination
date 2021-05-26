5694e888d3dfeb46084dfe7534f3ab0dcbff53bc
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 23:48:12 +0530

    Add : Triplet combination and its percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -12,31 +12,67 @@ if [[ $randomCheck1 -eq $isHead ]]
 then
 	if [[ $randomCheck2 -eq $isHead ]]
 	then
-		echo "Head is the winner"
-		dict[$x]=HH
-		x=$(($x+1))
-		i=$(($i+1))
+		if [[ $randomCheck2 -eq $isHead ]]
+	        then
+        	        echo "Head is the winner"
+                	dict[$x]=HHH
+                	x=$(($x+1))
+                	i=$(($i+1))
+        	else
+                	echo "Tail is the Winner"
+                	dict[$x]=HHT
+                	x=$(($x+1))
+                	j=$(($j+1))
+                	i=$(($i+1))
+		fi
 	else
-		echo "Tail is the Winner"
-        	dict[$x]=HT
-        	x=$(($x+1))
-        	j=$(($j+1))
-		i=$(($i+1))
+		if [[ $randomCheck2 -eq $isHead ]]
+	        then
+        	        echo "Head is the winner"
+                	dict[$x]=HTH
+                	x=$(($x+1))
+                	i=$(($i+1))
+			j=$(($j+1))
+        	else
+                	echo "Tail is the Winner"
+                	dict[$x]=HTT
+                	x=$(($x+1))
+                	j=$(($j+1))
+                	i=$(($i+1))
+		fi
 	fi
 else
-	if [[ $randomCheck2 -eq $isHead ]]
-        then
-                echo "Head is the winner"
-                dict[$x]=HT
-                x=$(($x+1))
-                i=$(($i+1))
-		j=$(($j+1))
-        else
-                echo "Tail is the Winner"
-                dict[$x]=TT
-                x=$(($x+1))
-                j=$(($j+1))
-        fi
+	if [[ $randomCheck2 -eq $isHead  ]]
+	then
+        	if [[ $randomCheck2 -eq $isHead ]]
+                then
+                        echo "Head is the winner"
+                        dict[$x]=THH
+                        x=$(($x+1))
+                        i=$(($i+1))
+			j=$(($j+1))
+                else
+                        echo "Tail is the Winner"
+                        dict[$x]=THT
+                        x=$(($x+1))
+                        j=$(($j+1))
+                        i=$(($i+1))
+                fi
+	else
+                if [[ $randomCheck2 -eq $isHead ]]
+                then
+                        echo "Head is the winner"
+                        dict[$x]=TTH
+                        x=$(($x+1))
+                        i=$(($i+1))
+                        j=$(($j+1))
+                else
+                        echo "Tail is the Winner"
+                        dict[$x]=TTT
+                        x=$(($x+1))
+                        i=$(($i+1))
+                fi
+	fi
 
 fi
 flip=$(($flip+1))
@@ -45,3 +81,4 @@ echo ${dict[@]}
 per=`awk 'BEGIN{print ('$j' / '$num') * 100}'`
 echo "Singlet percentage : $per"
 echo "Doublet percentage is : $per"
+echo "Triplet percentage is : $per"
