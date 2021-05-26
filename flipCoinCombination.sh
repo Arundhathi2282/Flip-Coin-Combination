b79b0e3eb5b07580ac52b71c9e76a77da93bf545
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 23:17:52 +0530

    Add : Singlet combination and its percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,26 @@
 #!/bin/bash -x
 isHead=1
+declare -A dict
+x=0
+flip=0
+read -p "How many times you want to flip a coin : " num
+while [[ $flip -lt $num ]]
+do
 randomCheck1=$((RANDOM%2))
 if [[ $randomCheck1 -eq $isHead ]]
 then
 	echo "Head is the winner"
+	dict[$x]=H
+	x=$(($x+1))
+	i=$(($i+1))
 else
 	echo "Tail is the Winner"
+	dict[$x]=T
+	x=$(($x+1))
+	j=$(($j+1))
 fi
+flip=$(($flip+1))
+done
+echo ${dict[@]}
+per=`awk 'BEGIN{print ('$j' / 10) * 100}'`
+echo "Singlet percentage : $per"
